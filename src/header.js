b97f65aa247bd965553d78c4559c102ffa675144
unknown <adam@.(none)>
2010-01-12 19:00:01 -0800

    * Refactored build.xml quite a bit. * Minification now uses Google Closure's advanced mode thanks to some multipass hackery.

---
+++ src/header.js
@@ -0,0 +1,11 @@
+/**
+* @preserve
+* Adamia 3D Engine v@VERSION@
+* Copyright (c) 2010 Adam R. Smith
+* Licensed under the new BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Project home: http://code.google.com/p/adamia-3d/
+* 
+* Date: @DATE@
+*/
