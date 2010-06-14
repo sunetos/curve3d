0ac1d37a7ac507da71ba7db2549084bbc2a38a87
unknown <adam@.(none)>
2010-06-13 20:16:02 -0700

    * Fixed the header documentation.

--- src/header.js
+++ src/header.js
@@ -1,11 +1,11 @@
 /**
 * @preserve
-* Adamia 3D Engine v@VERSION@
+* Curve3D Engine v@VERSION@
 * Copyright (c) 2010 Adam R. Smith
 * Licensed under the new BSD License:
 * http://www.opensource.org/licenses/bsd-license.php
 *
-* Project home: http://code.google.com/p/adamia-3d/
+* Project home: http://github.com/sunetos/curve3d
 * 
 * Date: @DATE@
 */
